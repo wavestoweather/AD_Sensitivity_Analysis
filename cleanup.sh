4682075346ab0bc93b6dfc58452eca58f6403764
Maicon Hieronymus <mahieron@Uni-Mainz.DE>
2021-04-20 12:48:37 +0200

    Removed old file.

--- cleanup.sh
+++
@@ -1,4 +0,0 @@
-rm /data/project/wcb/parquet/traj_stats2_testset/conv_400_median/*
-rm /data/project/wcb/parquet/traj_stats2_testset/conv_400_median_start_over/*
-rm /data/project/wcb/sim_results/traj_stats2_testset/conv_400_median_start_over/*
-rm /data/project/wcb/sim_results/traj_stats2_testset/conv_400_median/*
