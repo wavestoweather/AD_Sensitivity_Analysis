c1b578930282c057683f7a6db9812810f43bc703
Maicon Hieronymus <mahieron@Uni-Mainz.DE>
2021-09-24 16:55:03 +0200

    Updated requirements. Deleted not needed files.

--- exe_scripts/make_script.sh
+++
@@ -1,6 +0,0 @@
-cd ../build
-rm -r bin/ && rm CMakeCache.txt && rm -r CMakeFiles/ && rm -r src/ && rm Makefile && rm cmake_install.cmake
-cmake .. -DNETCDF_INCLUDE_DIR=/mnt/localscratch/include/ -DCODIPACK_INCLUDEDIR=/mnt/localscratch/include/ -DCMAKE_BUILD_TYPE=release -DTARGET=simulation && make -j4
-# -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DTRACE_QV:BOOL=ON
-# -DTRACE_QV:BOOL=ON -DTRACE_QR:BOOL=ON -DTRACE_SAT:BOOL=ON -DTRACE_TIME:BOOL=ON
-# -DTARGET=timing
