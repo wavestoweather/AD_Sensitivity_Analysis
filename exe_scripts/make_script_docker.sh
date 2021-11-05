b5be356be58632a9432db15edb01382962422ae5
Maicon Hieronymus <mahieron@Uni-Mainz.DE>
2021-11-05 15:30:47 +0100

    Make and test script are now marked as executables.

--- exe_scripts/make_script_docker.sh
+++ exe_scripts/make_script_docker.sh
@@ -3,4 +3,5 @@ then
     mkdir ../build
 fi
 cd ../build
+ls /usr/lib/x86_64-linux-gnu/
 cmake .. -DCMAKE_BUILD_TYPE=release -DTARGET=simulation  -DTRUSTED_DATA:BOOL=ON -DB_EIGHT:BOOL=ON && make -j4
