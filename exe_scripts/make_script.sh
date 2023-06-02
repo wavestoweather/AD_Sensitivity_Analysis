04f4bad0d5b86688abb9a3140ab7063f3cb0da94
Maicon Hieronymus <mhieronymus@uni-mainz.de>
2023-06-02 13:13:43 +0200

    Removed scripts and jobs not needed for the next release.

--- exe_scripts/make_script.sh
+++
@@ -1,3 +0,0 @@
-cd ../build
-rm -r bin/ && rm CMakeCache.txt && rm -r CMakeFiles/ && rm -r src/ && rm Makefile && rm cmake_install.cmake
-cmake .. -DTRUSTED_DATA:BOOL=ON -DTARGET=simulation && make -j 6
